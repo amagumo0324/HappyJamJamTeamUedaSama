b26a9d9629221656a50e618221a99620157cf208
amagumo0324 <94329649+amagumo0324@users.noreply.github.com>
2021-12-21 14:15:11 +0900

    kanayama

--- Assets/0_WorkSpace/Kanayama/Script/s_Player.cs
+++ Assets/0_WorkSpace/Kanayama/Script/s_Player.cs
@@ -4,29 +4,57 @@ using UnityEngine;
 
 public class s_Player : MonoBehaviour
 {
-    //
+    //tag受け取り用変数
     GameObject T_safe;
     GameObject T_out;
+
+    enum Check
+    {
+        Null,
+        Safe,
+        Out
+    };
+
+    Check check;
 
     // Start is called before the first frame update
     void Start()
     {
-        T_safe = GameObject.FindWithTag("");
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //tag確認
+        if (collision.tag == "safe")
+        {
+
+        }
+        else if(collision.tag == "out")
+        {
+
+        }
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        //マウス右クリック
-        if(Input.GetMouseButtonDown(1))
-        {
 
+        //マウス右クリック
+        if(Input.GetMouseButtonDown(1) && check == Check.Null)
+        {
+            check = Check.Safe;
         }
         //マウス左クリック
-        else if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) && check == Check.Null)
         {
-
+            check = Check.Out;
+        }
+        else
+        {
+            check = Check.Null;
         }
 
     }
+
 }
