8255b5ef50cf51021c2d9e605d30e5949450c437
amagumo0324 <94329649+amagumo0324@users.noreply.github.com>
2021-12-21 13:35:46 +0900

    kanayama

---
+++ Assets/0_WorkSpace/Kanayama/Script/s_Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //マウス右クリック
+        if(Input.GetMouseButtonDown(1))
+        {
+
+        }
+        //マウス左クリック
+        else if(Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
